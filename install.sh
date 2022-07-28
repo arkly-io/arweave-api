0233559f44d7a7f4064196dcacb5daaa3f216e3e
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2022-07-28 21:32:48 +0200

    Fixup requirements installation
    Uses requirements.txt over install.sh to install Python requirements.
    Requirements are pinned to a specific version as of today.

--- install.sh
+++
@@ -1,5 +0,0 @@
-python3 -m pip install fastapi
-python3 -m pip install "uvicorn[standard]"
-python3 -m pip install arweave-python-client
-python3 -m pip install python-multipart
-python3 -m pip install requests
