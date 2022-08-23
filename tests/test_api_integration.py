50edf316db094b05268be1e090fad090b16fa2e2
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2022-08-23 21:47:45 +0200

    Convert check balance test to VCRPY based test
    Introduces vcrpy to the unit tests and converts the first API
    integration test to use it.

--- tests/test_api_integration.py
+++ tests/test_api_integration.py
@@ -2,19 +2,48 @@
 To lint use:
 tox -e linting
 """
+
+from pathlib import Path
+from typing import Final
+
 import pytest
 import requests
+import vcr
 
 response = requests.get("http://127.0.0.1:8000/")
+
+# Location to record VCR cassettes to replay API requests/responses.
+VCR_FIXTURES_PATH: Final[Path] = Path("tests/fixtures/vcrpy/")
+
+# Wallet name will need to exist when VCR cassettes are re-recorded.
+WALLET_NAME: Final[Path] = Path("myWallet.json")
+
+
+def _scrub_wallet_data(replace=True):
+    """Ensure that wallet data is removed from vcrpy requests."""
+
+    def before_record_request(request):
+        if not replace:
+            # View the raw request data for debugging purposes. Will only
+            # work when the cassette is re-recorded.
+            return request
+        request.body = "substitute: my_wallet.json -- form-data"
+        return request
+
+    return before_record_request
 
 
 def test_check_balance():
     """Testing the check_balance endpoint"""
-    with open("myWallet.json", "rb") as my_file:
-        files = {"file": ("myWallet.json", my_file)}
-        req = requests.post(url="http://127.0.0.1:8000/check_balance/", files=files)
-        # self.assertNotEqual(req.text, None)
-        assert req.text != None
+    arweave_vcr = vcr.VCR(before_record_request=_scrub_wallet_data())
+    with arweave_vcr.use_cassette(
+        str(VCR_FIXTURES_PATH / Path("test_check_balance.yaml"))
+    ):
+        with open(str(WALLET_NAME), "rb") as my_file:
+            files = {"file": (str(WALLET_NAME), my_file)}
+            req = requests.post(url="http://127.0.0.1:8000/check_balance/", files=files)
+            # self.assertNotEqual(req.text, None)
+            assert req.text == '{"balance":0.01949816252}'
 
 
 def test_check_last_transaction():
